6452959b09d10a3c6327d187405e167385d1d3ae
Adeema Amir <adeemaamir1722@gmail.com>
2023-05-02 13:52:21 +0500

    second

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,69 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+// first api
+
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(request: NextRequest) {
+
+//   const url = request.nextUrl;
+
+//   // return new NextResponse('Hello, World');
+
+//   if (url.searchParams.has("name")) {
+//     const name = url.searchParams.get("name");
+//     return NextResponse.json({message : 'Hello, from: ' + name});
+//   }
+//   else {
+//     return new NextResponse('Please send your name in search parameter "name"');
+//   }
+
+// }
+
+// second api
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    return NextResponse.json({
+      From: "Zia",
+      Message: "Greetings from Pakistan",
+      RequestType: "GET"
+    });
+}
+
+export async function POST(request: NextRequest) {
+  const req = await request.json();
+  if(req.name){
+    return NextResponse.json({
+      To: req.name,
+      Message: "All well here",
+      RequestType: "POST"
+    });
+  }
+  else {
+    return new NextResponse('Please include your name in the POST request');
+  }
+}
+
+export async function PUT(request: NextRequest) {
+  const req = await request.json();
+  if(req.name){
+    return NextResponse.json({
+      To: req.name,
+      Message: "This is a updated greeting",
+      RequestType: "PUT"
+    });
+  }
+  else {
+    return new NextResponse('Please include your name in the PUT request');
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+ 
+    return NextResponse.json({
+      To: "Zia",
+      Message: "I have deleted the greetings",
+      RequestType: "DELETE"
+    });
+
 }
